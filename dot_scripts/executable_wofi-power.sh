6c59f88ba5df7f7219ab6422e03ded6768906f49
Marvin Wunderlich <opidopiopi@gmail.com>
2023-05-21 15:07:24 +0200

    Add special workspace bindings and scripts dir

---
+++ dot_scripts/executable_wofi-power.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+entries=" Lock\n⇠ Logout\n⏾ Suspend\n⭮ Reboot\n⏻ Shutdown"
+
+selected=$(echo -e $entries|wofi --width 250 --height 210 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
+
+case $selected in
+  lock)
+    exec swaylock --image ~/files/images/SBKjnxm.jpg;;
+    #exec .scripts/blur-lock.sh;;
+  logout)
+    hyprctl dispatch exit;;
+  suspend)
+    exec systemctl suspend;;
+  reboot)
+    exec systemctl reboot;;
+  shutdown)
+    exec systemctl poweroff -i;;
+esac
